2d6d606071b2ad6b681792e0ca2374ab80abaf9d
daviderovelli <daviderovelli24@gmail.com>
2020-04-01 19:59:46 +0100

    kissFFT test and plot on sample data

--- audio_processing/kiss_fft_test.cpp
+++ audio_processing/kiss_fft_test.cpp
@@ -6,28 +6,46 @@
 
 
 int main(){
-    char line[100];
+    char line[10];
     // number of samples
-    int nfft = 1024000;
+    int nfft = 1024;
+    // Open file with data from the ADC 
     std::ifstream f;
     f.open("samples/sample_sines.txt", std::ifstream::in);
+    // Open output FFT file
+    std::ofstream out;
+    out.open("samples/sine_phases.txt", std::ofstream::out);
 
-    /*kiss_fftr_cfg cfg = kiss_fftr_alloc(nfft, 0, 0, 0);
+
+    // Real valued FFT data structures init. 
+    kiss_fftr_cfg cfg = kiss_fftr_alloc(nfft, 0, 0, 0);
     kiss_fft_scalar *cx_in = new kiss_fft_scalar[nfft];
-    kiss_fft_cpx *cx_out = new kiss_fft_cpx[nfft/2+1];*/
-
-    f.getline(line, 100); 
-    for (int k = 0; k < nfft; ++k)
+    kiss_fft_cpx *cx_out = new kiss_fft_cpx[nfft/2+1];
+ 
+    while (f.getline(line, 10))
     {
-        std::cout << line;
-        f.getline(line, 100); 
+        for (int i = 0; i < nfft; i++){
+            cx_in[i] = std::stod(line);
+            std::cout << cx_in[i];
+            if (!f.getline(line, 10)) {
+                break;
+            }
+        }
+
+        //fft here
+        kiss_fftr( cfg , cx_in , cx_out );
+
+        // print it to file here
+        for (int k = 0; k < nfft/2+1; k++){
+            out << cx_out[k].i;
+            out << "\n";
+        }
+        
+
+
     }
-
-    /*kiss_fftr(cfg, cx_in, cx_out);
-
-    printf(cx_out);
-
+    out.close();
     free(cfg);
     delete[] cx_in;
-    delete[] cx_out;*/
+    delete[] cx_out;
 }
