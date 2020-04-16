e68bd9858acb7971ac582afa1cfd4c82cb448adf
daviderovelli <daviderovelli24@gmail.com>
2020-04-16 18:29:35 +0100

    EQ speed improvement, fully RT reactive

--- shaders/EQ.glsl
+++ shaders/EQ.glsl
@@ -1,33 +1,30 @@
 uniform float H;
 uniform float W;
 uniform float fft[513];
-
+// this "cuts down" the fft. 513 (MAX) for full resolution
+// 340 bins will display the first 16KHz that is a good 
+// spectrum for most music applications
+const float n_of_bins = 340.0;
 
 void main( void ) {
-	
+	// normalise
 	float X=gl_FragCoord.x/W;
     float Y=gl_FragCoord.y/H;
 	
-	float color = 0.0;
-	float c= 0.0;
-	
-	
-	for(int i=0; i < 513; i+=8){
-	
-		if(mod(X,1.0/32.0)<0.02 && X<c/32.0 && X > (c-1.0)/32.0){
-		
-			if(Y<fft[i]){
-			
-				color+=0.5;
-				
-			}
-			
-		}
-		
-		c+=1.0;
-	}
-	
+	float level = 0.0;
+	float xstep=1.0/n_of_bins;
 
-	gl_FragColor = vec4( vec3( color, color,color ), 1.0 );
+	float rel_pos=X/xstep;
+
+	// Casting creates the bin. i.e. all rel_pos values between 1.0 and 1.99 
+	// (and therefore their corresponding X values) will have a Y value of fft[1]
+	int bin=int(rel_pos);
+
+	if (Y <= fft[bin]){
+		level += 1.0;
+	}
+		
+
+	gl_FragColor = vec4( vec3(level), 1.0 );
 
 }
